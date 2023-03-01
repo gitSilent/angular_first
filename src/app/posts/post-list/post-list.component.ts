3f89a12ac636bd58ca2b469cf0d3fe4b4bd015e9
40kabinet <weqwe@ewe.com>
2023-03-01 10:57:41 +0300

    init commit base functions

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from "@angular/core"
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+
+// posts:{
+//     title:string,
+//     content:string
+// }[] = []
+
+export class PostListComponent{
+    @Input() posts: any[] = []
+}
